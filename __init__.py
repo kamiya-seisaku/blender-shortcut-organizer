a7d05e55af19da7cc49aa1844bfbc5a39276bc18
kkay <spgt417+github@gmail.com>
2023-10-17 05:46:45 +0900

    restart from another template

--- __init__.py
+++ __init__.py
@@ -1,54 +1,92 @@
-__bl_info__ = {
+bl_info = {
     "name": "Blender Shortcut Organizer",
-    "description": "Simplifies the management of shortcuts in Blender.",
     "author": "kkay",
     "version": (1, 0),
-    "blender": (3, 6, 2),
+    "blender": (2, 80, 0),
     "location": "View3D > Tool Shelf > Shortcut Organizer",
+    "description": "Simplifies the management of shortcuts in Blender",
     "warning": "",
     "doc_url": "",
-    "category": "3D View"
+    "category": "3D View",
 }
 
-import bpy
 
-# Main Class for the Addon
-class ShortcutOrganizer(bpy.types.Operator):
-    bl_idname = "object.shortcut_organizer"
-    bl_label = "Shortcut Organizer"
-    
-    # Intuitive UI
-    def draw(self, context):
-        layout = self.layout
-        # UI elements go here
-    
-    # Direct Assignment
-    def assign_shortcut(self, button):
-        # Code for direct assignment
-    
-    # Shortcut Input Prompt
-    def input_prompt(self):
-        # Code for input prompt
-    
-    # Conflict Resolution
-    def resolve_conflict(self, key):
-        # Code for conflict resolution
-    
-    # Alternative Suggestions
-    def suggest_alternatives(self, key):
-        # Code for alternative suggestions
-    
-    # Final Assignment
-    def finalize_assignment(self, key):
-        # Code for finalizing assignment
+import bpy
+from bpy.types import Operator
+from bpy.props import FloatVectorProperty
+from bpy_extras.object_utils import AddObjectHelper, object_data_add
+from mathutils import Vector
+
+
+def add_object(self, context):
+    scale_x = self.scale.x
+    scale_y = self.scale.y
+
+    verts = [
+        Vector((-1 * scale_x, 1 * scale_y, 0)),
+        Vector((1 * scale_x, 1 * scale_y, 0)),
+        Vector((1 * scale_x, -1 * scale_y, 0)),
+        Vector((-1 * scale_x, -1 * scale_y, 0)),
+    ]
+
+    edges = []
+    faces = [[0, 1, 2, 3]]
+
+    mesh = bpy.data.meshes.new(name="New Object Mesh")
+    mesh.from_pydata(verts, edges, faces)
+    # useful for development when the mesh may be invalid.
+    # mesh.validate(verbose=True)
+    object_data_add(context, mesh, operator=self)
+
+
+class OBJECT_OT_add_object(Operator, AddObjectHelper):
+    """Create a new Mesh Object"""
+    bl_idname = "mesh.add_object"
+    bl_label = "Add Mesh Object"
+    bl_options = {'REGISTER', 'UNDO'}
+
+    scale: FloatVectorProperty(
+        name="scale",
+        default=(1.0, 1.0, 1.0),
+        subtype='TRANSLATION',
+        description="scaling",
+    )
+
+    def execute(self, context):
+
+        add_object(self, context)
+
+        return {'FINISHED'}
+
 
 # Registration
 
+def add_object_button(self, context):
+    self.layout.operator(
+        OBJECT_OT_add_object.bl_idname,
+        text="Add Object",
+        icon='PLUGIN')
+
+
+# This allows you to right click on a button and link to documentation
+def add_object_manual_map():
+    url_manual_prefix = "https://docs.blender.org/manual/en/latest/"
+    url_manual_mapping = (
+        ("bpy.ops.mesh.add_object", "scene_layout/object/types.html"),
+    )
+    return url_manual_prefix, url_manual_mapping
+
+
 def register():
-    bpy.utils.register_class(ShortcutOrganizer)
+    bpy.utils.register_class(OBJECT_OT_add_object)
+    bpy.utils.register_manual_map(add_object_manual_map)
+    bpy.types.VIEW3D_MT_mesh_add.append(add_object_button)
+
 
 def unregister():
-    bpy.utils.unregister_class(ShortcutOrganizer)
+    bpy.utils.unregister_class(OBJECT_OT_add_object)
+    bpy.utils.unregister_manual_map(add_object_manual_map)
+    bpy.types.VIEW3D_MT_mesh_add.remove(add_object_button)
 
 
 if __name__ == "__main__":
