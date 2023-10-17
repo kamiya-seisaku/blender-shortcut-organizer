602841795782ce45d651ac294362cd5524d5082c
kkay <spgt417+github@gmail.com>
2023-10-17 16:10:24 +0900

    debug mode and reload button

--- __init__.py
+++ __init__.py
@@ -62,92 +62,40 @@ class HelloWorldPanel(bpy.types.Panel):
     bl_context = "object"
 
     def draw(self, context):
-        self.layout.label(text="Hello World")
+        layout = self.layout  # Define layout here
+        layout.label(text="Hello World")
+
+        # Add Debug Mode checkbox
+        layout.prop(context.scene, "debug_mode", text="Debug Mode")
+
+        # Conditionally show Reload Addon button
+        if context.scene.debug_mode:
+            layout.operator("object.reload_addon", text="Reload Addon")
+
+# New Reload Addon Operator
+class ReloadAddonOperator(bpy.types.Operator):
+    bl_idname = "object.reload_addon"
+    bl_label = "Reload Addon"
+
+    def execute(self, context):
+        bpy.ops.preferences.addon_disable(module="your_addon_module_name")
+        bpy.ops.preferences.addon_enable(module="your_addon_module_name")
+        return {'FINISHED'}
 
 # Registration
 
 def register():
     bpy.utils.register_class(ShortcutOrganizer)
     bpy.utils.register_class(HelloWorldPanel)
+    bpy.types.Scene.debug_mode = bpy.props.BoolProperty(name="Debug Mode")
+    bpy.utils.register_class(ReloadAddonOperator)
+    bpy.utils.register_class(ReloadAddonOperator)  # Register the new class
 
 def unregister():
     bpy.utils.unregister_class(ShortcutOrganizer)
     bpy.utils.unregister_class(HelloWorldPanel)
+    bpy.utils.unregister_class(ReloadAddonOperator)  # Unregister the new class
 
 
 if __name__ == "__main__":
     register()
-
-# def add_object(self, context):
-#     scale_x = self.scale.x
-#     scale_y = self.scale.y
-
-#     verts = [
-#         Vector((-1 * scale_x, 1 * scale_y, 0)),
-#         Vector((1 * scale_x, 1 * scale_y, 0)),
-#         Vector((1 * scale_x, -1 * scale_y, 0)),
-#         Vector((-1 * scale_x, -1 * scale_y, 0)),
-#     ]
-
-#     edges = []
-#     faces = [[0, 1, 2, 3]]
-
-#     mesh = bpy.data.meshes.new(name="New Object Mesh")
-#     mesh.from_pydata(verts, edges, faces)
-#     # useful for development when the mesh may be invalid.
-#     # mesh.validate(verbose=True)
-#     object_data_add(context, mesh, operator=self)
-
-
-# class OBJECT_OT_add_object(Operator, AddObjectHelper):
-#     """Create a new Mesh Object"""
-#     bl_idname = "mesh.add_object"
-#     bl_label = "Add Mesh Object"
-#     bl_options = {'REGISTER', 'UNDO'}
-
-#     scale: FloatVectorProperty(
-#         name="scale",
-#         default=(1.0, 1.0, 1.0),
-#         subtype='TRANSLATION',
-#         description="scaling",
-#     )
-
-#     def execute(self, context):
-
-#         add_object(self, context)
-
-#         return {'FINISHED'}
-
-
-# # Registration
-
-# def add_object_button(self, context):
-#     self.layout.operator(
-#         OBJECT_OT_add_object.bl_idname,
-#         text="Add Object",
-#         icon='PLUGIN')
-
-
-# # This allows you to right click on a button and link to documentation
-# def add_object_manual_map():
-#     url_manual_prefix = "https://docs.blender.org/manual/en/latest/"
-#     url_manual_mapping = (
-#         ("bpy.ops.mesh.add_object", "scene_layout/object/types.html"),
-#     )
-#     return url_manual_prefix, url_manual_mapping
-
-
-# def register():
-#     bpy.utils.register_class(OBJECT_OT_add_object)
-#     bpy.utils.register_manual_map(add_object_manual_map)
-#     bpy.types.VIEW3D_MT_mesh_add.append(add_object_button)
-
-
-# def unregister():
-#     bpy.utils.unregister_class(OBJECT_OT_add_object)
-#     bpy.utils.unregister_manual_map(add_object_manual_map)
-#     bpy.types.VIEW3D_MT_mesh_add.remove(add_object_button)
-
-
-# if __name__ == "__main__":
-#     register()
