8d52bfb747eb208adbfad03a1e7f2feff6493e72
kkay <spgt417+github@gmail.com>
2023-10-23 15:27:09 +0900

    classes separated to new py

---
+++ shortcut_organizer_classes.py
@@ -0,0 +1,119 @@
+# Imports
+if "bpy" in locals():
+# Registration/Unregistration
+    from .state_machine import BlenderAddonStateMachine
+    pass
+else:
+    import bpy
+
+# Globals
+context_menu_types = [menu for menu in dir(bpy.types) if menu.endswith('_context_menu')]
+
+# Initialize state machine
+sm = BlenderAddonStateMachine()
+
+# Popup Window Operator
+class ShortcutOrganizerPopupOperator(bpy.types.Operator):
+    bl_idname = "object.shortcut_organizer_popup"
+    bl_label = "Shortcut Organizer Popup"
+    proposed_keys = bpy.props.StringProperty()
+    
+    # Event history.
+    # Format: [time, event_type, modifiers, repeat_count]
+    event_history = []
+    # Operator history.
+    # Format: [time, bl_label, idname_py, addr]
+    operator_history = []
+
+    @classmethod
+    def poll(cls, context):
+        # Always active
+        return True
+
+    def modal(self, context, event):
+        if event.type == 'TIMER':  # Timer event
+            context.area.tag_redraw()  # Force a redraw of the area to update the UI
+
+        if event.type == 'ESC':  # Cancel
+            return {'CANCELLED'}
+        elif event.type == 'RET':  # Confirm
+            return {'FINISHED'}
+        
+        if event.value == 'PRESS':
+            if self.proposed_keys == "Press key to assign.  Type F11 to finish.":
+                self.proposed_keys = ""
+            self.proposed_keys += event.type
+            print("User pressed:", event.type)
+        else:  # Capture any other key
+            self.report({'INFO'}, f"Captured key: {event.type}")
+            return {'RUNNING_MODAL'}
+        return {'RUNNING_MODAL'}
+
+    def invoke(self, context, event):
+        context.window_manager.modal_handler_add(self)  #Initiate the modal operation
+        self.proposed_keys = "Press key to assign.  Type F11 to finish."
+        return context.window_manager.invoke_props_dialog(self)
+
+    def draw(self, context):
+        layout = self.layout
+        layout.label(text=f"{self.proposed_keys}")  # Display proposed keys
+        layout.operator("object.reload_addon", text="Reload Addon")
+        layout.operator("object.assign_key", text="Assign")  # Add Assign button
+
+    def execute(self, context):
+        self.report({'INFO'}, "Popup Opened")
+        return {'FINISHED'}
+
+# Assign Key Operator
+class AssignKeyOperator(bpy.types.Operator):
+    bl_idname = "object.assign_key"
+    bl_label = "Assign Key"
+
+    def execute(self, context):
+        # Your code to assign the key stroke
+        return {'FINISHED'}
+
+# Main Class for the Addon
+class ShortcutOrganizer(bpy.types.Operator):
+    bl_idname = "object.shortcut_organizer"
+    bl_label = "Shortcut Organizer"
+
+    def draw(self, context):
+        layout = self.layout
+        pass
+
+    # Todo: Functionality implementations
+
+# Panel class for the Property Window
+class OBJECT_PT_ShortcutOrganizerPropertyPanel(bpy.types.Panel):
+    bl_idname = "OBJECT_PT_ShortcutOrganizerPropertyPanel"
+    bl_label = "Shortcut Organizer"
+    bl_space_type = 'PROPERTIES'
+    bl_region_type = 'WINDOW'
+    bl_context = "object"
+
+    def draw(self, context):
+        # layout = self.layout
+        # layout.label(text="Shortcut Organizer in Property Panel")
+        # layout.operator("object.shortcut_organizer_popup", text="Open Shortcut Organizer")
+        layout = self.layout
+        # layout.label(text="Press key to assign")
+        layout.operator("object.reload_addon", text="Reload Addon")
+        # layout.operator("object.assign_key", text="Assign")  # Add Assign button
+
+# Context menu to add Popup
+def add_context_menu(self, context):
+    self.layout.operator('object.shortcut_organizer_popup')
+
+# Reload Addon Operator
+class ReloadAddonOperator(bpy.types.Operator):
+    bl_idname = "object.reload_addon"
+    bl_label = "Reload Addon"
+
+    def execute(self, context):
+        bpy.ops.preferences.addon_disable(module="blender-shortcut-organizer")
+        bpy.ops.preferences.addon_enable(module="blender-shortcut-organizer")
+        return {'FINISHED'}
+
+classes = [ShortcutOrganizer, OBJECT_PT_ShortcutOrganizerPropertyPanel, ReloadAddonOperator, AssignKeyOperator, ShortcutOrganizerPopupOperator]
+
